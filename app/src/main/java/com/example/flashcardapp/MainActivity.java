0adb9df078f52de791d91c89cb6c9d27d741f74a
Wendy Chen <wendychen0@users.noreply.github.com>
2021-02-26 23:11:16 -0800

    Added optional features to toggle answer visibility and show correct/incorrect answers

--- app/src/main/java/com/example/flashcardapp/MainActivity.java
+++ app/src/main/java/com/example/flashcardapp/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,11 +16,69 @@ public class MainActivity extends AppCompatActivity {
 
         TextView flashcardQuestion = findViewById(R.id.flashcard_question);
         TextView flashcardAnswer = findViewById(R.id.flashcard_answer);
+        TextView flashcardAnswer1 = findViewById(R.id.flashcard_answer1);
+        TextView flashcardAnswer2 = findViewById(R.id.flashcard_answer2);
+        TextView flashcardAnswer3 = findViewById(R.id.flashcard_answer3);
+        ImageView toggleView = findViewById(R.id.toggle_choices_visibility);
+        ImageView toggleViewOpen = findViewById(R.id.toggle_choices_visibility_open);
         flashcardQuestion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 flashcardQuestion.setVisibility(View.INVISIBLE);
                 flashcardAnswer.setVisibility(View.VISIBLE);
+            }
+        });
+
+        flashcardAnswer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                flashcardQuestion.setVisibility(View.VISIBLE);
+                flashcardAnswer.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        flashcardAnswer1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                flashcardAnswer1.setBackgroundColor(getResources().getColor(R.color.nice_red, null));
+            }
+        });
+
+        flashcardAnswer2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                flashcardAnswer2.setBackgroundColor(getResources().getColor(R.color.nice_red, null));
+            }
+        });
+
+        flashcardAnswer3.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                flashcardAnswer3.setBackgroundColor(getResources().getColor(R.color.green, null));
+            }
+        });
+
+        toggleView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((ImageView) findViewById(R.id.toggle_choices_visibility)).setImageResource(R.drawable.ic_eye_open);
+                boolean isShowingAnswers;
+                flashcardAnswer1.setVisibility(View.VISIBLE);
+                flashcardAnswer2.setVisibility(View.VISIBLE);
+                flashcardAnswer3.setVisibility(View.VISIBLE);
+                toggleViewOpen.setVisibility(View.VISIBLE);
+            }
+        });
+
+        toggleViewOpen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((ImageView) findViewById(R.id.toggle_choices_visibility)).setImageResource(R.drawable.ic_eye_slash);
+                flashcardAnswer1.setVisibility(View.INVISIBLE);
+                flashcardAnswer2.setVisibility(View.INVISIBLE);
+                flashcardAnswer3.setVisibility(View.INVISIBLE);
+                toggleViewOpen.setVisibility(View.INVISIBLE);
+
             }
         });
     }
