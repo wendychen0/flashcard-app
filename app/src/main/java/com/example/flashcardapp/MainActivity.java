4601ba0808edb5d2986e46e08c88c48063eabd8b
Wendy Chen <wendy.chen5272@gmail.com>
2021-03-19 22:45:16 -0700

    implemented saving cards/database feature

--- app/src/main/java/com/example/flashcardapp/MainActivity.java
+++ app/src/main/java/com/example/flashcardapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.flashcardapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,12 +9,28 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    FlashcardDatabase flashcardDatabase;
+    List<Flashcard> allFlashcards;
+
+    int currentCardDisplayedIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        flashcardDatabase = new FlashcardDatabase(getApplicationContext());
+        allFlashcards = flashcardDatabase.getAllCards();
+
+        if (allFlashcards != null && allFlashcards.size() > 0) {
+            ((TextView) findViewById(R.id.flashcard_question)).setText(allFlashcards.get(0).getQuestion());
+            ((TextView) findViewById(R.id.flashcard_answer)).setText(allFlashcards.get(0).getAnswer());
+        }
 
         TextView newQuestion = findViewById(R.id.questionTextField);
         TextView newAnswer = findViewById(R.id.answerTextField);
@@ -93,6 +110,37 @@ public class MainActivity extends AppCompatActivity {
                 MainActivity.this.startActivityForResult(intent, 100);
             }
         });
+
+        findViewById(R.id.nextButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (allFlashcards.size() == 0)
+                    return;
+                currentCardDisplayedIndex += 1;
+                if(currentCardDisplayedIndex >= allFlashcards.size()) {
+                    Snackbar.make(flashcardQuestion,
+                            "You've reached the end of the cards, going back to start.",
+                            Snackbar.LENGTH_SHORT).show();
+                    currentCardDisplayedIndex = 0;
+                }
+                allFlashcards = flashcardDatabase.getAllCards();
+                Flashcard flashcard = allFlashcards.get(currentCardDisplayedIndex);
+
+                ((TextView) findViewById(R.id.flashcard_question)).setText(flashcard.getQuestion());
+                ((TextView) findViewById(R.id.flashcard_answer)).setText(flashcard.getAnswer());
+            }
+        });
+        findViewById(R.id.trashButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                flashcardDatabase.deleteCard(((TextView) findViewById(R.id.flashcard_question)).getText().toString());
+                currentCardDisplayedIndex += 1;
+                allFlashcards = flashcardDatabase.getAllCards();
+                if (allFlashcards.size() == 1)
+                    ((TextView) findViewById(R.id.flashcard_question)).setText("Add a card!");
+
+            }
+        });
     }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -104,6 +152,8 @@ public class MainActivity extends AppCompatActivity {
 
             ((TextView)findViewById(R.id.flashcard_answer)).setText(string2);
 
+            flashcardDatabase.insertCard(new Flashcard(string1, string2));
+            allFlashcards = flashcardDatabase.getAllCards();
         }
     }
 }
